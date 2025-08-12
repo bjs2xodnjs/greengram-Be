6f37d409dcb678b0d3f86cd8b89e6e54ef64e21d
taewon1918 <taewon1918@gmail.com>
2025-08-12 11:15:25 +0900

    FeedPostReq message 추가

--- src/main/java/com/green/greengram/application/feed/model/FeedPostReq.java
+++ src/main/java/com/green/greengram/application/feed/model/FeedPostReq.java
@@ -9,9 +9,9 @@ import lombok.ToString;
 @ToString
 @Setter
 public class FeedPostReq {
-    @Size(max = 1000)
+    @Size(max = 1_000, message = "contents는 1,000자 이하여야 합니다.")
     private String contents;
 
-    @Size(max = 30)
+    @Size(max = 30, message = "location은 30 이하여야 합니다.")
     private String location;
 }
